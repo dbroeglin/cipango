57aa71852474753b36525cbdaa16e10f96574856
nicolas@cipango.org <nicolas@cipango.org@657f710f-be33-0410-a596-71a6d81f68ef>
2010-01-29 10:45:14 +0000

    load tests
    git-svn-id: https://cipango.googlecode.com/svn/trunk@177 657f710f-be33-0410-a596-71a6d81f68ef

---
+++ extras/load-sipapp/src/main/java/org/cipango/test/B2bServlet.java
@@ -0,0 +1,88 @@
+// ========================================================================
+// Copyright 2008-2010 NEXCOM Systems
+// ------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ========================================================================
+package org.cipango.test;
+
+import java.io.*;
+import java.util.List;
+
+import javax.servlet.*;
+import javax.servlet.sip.*;
+
+public class B2bServlet extends SipServlet
+{
+
+	@Override
+	protected void doRequest(SipServletRequest request) throws ServletException, IOException
+	{
+		B2buaHelper helper = request.getB2buaHelper();
+		SipServletRequest newRequest;
+		if (request.isInitial())
+		{
+			newRequest = helper.createRequest(request);
+			newRequest.getSession().setHandler(getServletName());
+		}
+		else
+		{
+			SipSession session = helper.getLinkedSession(request.getSession());
+			if ("ACK".equals(request.getMethod()))
+			{
+				List<SipServletMessage> l = helper.getPendingMessages(session, UAMode.UAC);
+				SipServletResponse response = (SipServletResponse) l.get(0);
+				newRequest = response.createAck();
+			}
+			else if ("CANCEL".equals(request.getMethod()))
+			{
+				newRequest = helper.createCancel(session);
+			}
+			else
+			{
+				newRequest = helper.createRequest(session, request, null);
+			}
+			copyContent(request, newRequest);
+		}
+
+		newRequest.send();
+	}
+
+
+
+	protected void doResponse(SipServletResponse response)
+	{
+		try
+		{
+			B2buaHelper helper = response.getRequest().getB2buaHelper();
+			SipServletRequest linkedRequest = helper
+					.getLinkedSipServletRequest(response.getRequest());
+			SipServletResponse newResponse = linkedRequest.createResponse(
+					response.getStatus(), response.getReasonPhrase());
+			copyContent(response, newResponse);
+			newResponse.send();
+		}
+		catch (Throwable e)
+		{
+			log("Failed to handle:\n" + response, e);
+		}
+	}
+	
+	private void copyContent(SipServletMessage msg1, SipServletMessage msg2) {
+		try {
+			if (msg1.getContentLength() != 0) {
+				msg2.setContent(msg1.getRawContent(), msg1.getContentType());
+			}
+		} catch (Exception e) {
+			log("Unable to copy content", e);
+		}
+	}
+
+}

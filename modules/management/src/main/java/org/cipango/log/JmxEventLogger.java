3e80c785cd734eeaf6cf2634d3fdc0ffacedfa7a
nicolas@cipango.org <nicolas@cipango.org@657f710f-be33-0410-a596-71a6d81f68ef>
2010-03-30 16:26:28 +0000

    SNMP prototype
    git-svn-id: https://cipango.googlecode.com/svn/trunk@227 657f710f-be33-0410-a596-71a6d81f68ef

---
+++ modules/management/src/main/java/org/cipango/log/JmxEventLogger.java
@@ -0,0 +1,54 @@
+// ========================================================================
+// Copyright 2010 NEXCOM Systems
+// ------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ========================================================================
+package org.cipango.log;
+
+import java.lang.management.ManagementFactory;
+
+import javax.management.Notification;
+import javax.management.NotificationBroadcasterSupport;
+import javax.management.ObjectName;
+
+public class JmxEventLogger extends NotificationBroadcasterSupport implements EventLogger, JmxEventLoggerMBean
+{
+
+	private long _sequenceNumber = 0;
+	
+	public JmxEventLogger()
+	{
+		try
+		{
+			ManagementFactory.getPlatformMBeanServer().registerMBean(this,
+					new ObjectName("org.cipango.log:type=jmxeventlogger,id=0"));
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	
+	public void log(int eventType, String message)
+	{
+		Notification notification = new Notification(String.valueOf(eventType), this, _sequenceNumber++, message);
+		sendNotification(notification);
+	}
+
+
+	public long getSequenceNumber()
+	{
+		return _sequenceNumber;
+	}
+
+}

1d8ca976550c1103788e597653afa26a6c10926c
dominique.broeglin <dominique.broeglin@657f710f-be33-0410-a596-71a6d81f68ef>
2010-09-07 21:11:07 +0000

    Refactoring of some verbose examples
    git-svn-id: https://cipango.googlecode.com/svn/trunk@423 657f710f-be33-0410-a596-71a6d81f68ef

--- extras/sipatra/spec/base_spec.rb
+++ extras/sipatra/spec/base_spec.rb
@@ -103,7 +103,8 @@ end
 describe Sipatra::Base do
   [:ack, :bye, :cancel, :info, :invite, :message, 
     :notify, :options, :prack, :publish, :refer, 
-    :register, :subscribe, :update, :request].each do |name|
+    :register, :subscribe, :update, 
+    :request, :response, :helpers].each do |name|
     it "should accept method handler #{name}" do
       Sipatra::Base.respond_to?(name).should be_true
     end
@@ -167,245 +168,230 @@ describe TestApp do
   end
 end
 
-describe 'Sipatra::Base params ' do
-  
-  subject do
-    app_class = Class::new(Sipatra::Base)
-    app = app_class.new
-    app.msg = mock_request('INVITE', 'sip:+uri-1-2-3:pass@domain.com;params1=test')
-    app
+describe Sipatra::Base do
+  before do
+    @app = Class::new(Sipatra::Base).new
   end
   
-  it 'should not have an empty size by default' do
-    subject.params.size.should == 0
-  end
+  subject { @app }
   
-  describe "when receiving do_request (with URI sip:+uri-1-2-3:pass@domain.com;params1=test)" do
-    after do
-      subject.do_request
+  describe 'Sipatra::Base params ' do
+  
+    before do
+      @app.msg = mock_request('INVITE', 'sip:+uri-1-2-3:pass@domain.com;params1=test')
     end
+  
+    it 'should not have an empty size by default' do
+      subject.params.size.should == 0
+    end
+  
+    describe "when receiving do_request (with URI sip:+uri-1-2-3:pass@domain.com;params1=test)" do
+      after do
+        subject.do_request
+      end
     
-    it "should pass processing through a right matching string " do
-      subject.class.invite('sip:(:uri):(:pass)@(:domain);.*') do
-        must_be_called
-      end
+      it "should pass processing through a right matching string " do
+        subject.class.invite('sip:(:uri):(:pass)@(:domain);.*') do
+          must_be_called
+        end
       
-      subject.should_receive(:must_be_called)
-    end
+        subject.should_receive(:must_be_called)
+      end
     
-    it "should pass processing through a right regexp " do
-      subject.class.invite(/sip:.*:.*@.*;.*/) do
-        must_be_called
-      end
+      it "should pass processing through a right regexp " do
+        subject.class.invite(/sip:.*:.*@.*;.*/) do
+          must_be_called
+        end
       
-      subject.should_receive(:must_be_called)
-    end
+        subject.should_receive(:must_be_called)
+      end
     
-    it "should not be processed through a wrong regexp " do
-      subject.class.invite('sip:domain.com') do
-        must_not_be_called
-      end
+      it "should not be processed through a wrong regexp " do
+        subject.class.invite('sip:domain.com') do
+          must_not_be_called
+        end
       
-      subject.should_not_receive(:must_not_be_called)
+        subject.should_not_receive(:must_not_be_called)
+      end
     end
-  end
   
-  describe "when receiving do_request" do
-    it "should have access to params between brackets" do
-      subject.class.invite('sip:(:user):(:pass)?@(:domain);.*') do
-        must_be_called
-      end
-      subject.should_receive(:must_be_called)
-      subject.do_request
+    describe "when receiving do_request" do
+      it "should have access to params between brackets" do
+        subject.class.invite('sip:(:user):(:pass)?@(:domain);.*') do
+          must_be_called
+        end
+        subject.should_receive(:must_be_called)
+        subject.do_request
       
-      subject.params[:user].should == "+uri-1-2-3"
-      subject.params[:pass].should == "pass"
-      subject.params[:domain].should == "domain.com"
-      subject.params[:uri].should == nil
+        subject.params[:user].should == "+uri-1-2-3"
+        subject.params[:pass].should == "pass"
+        subject.params[:domain].should == "domain.com"
+        subject.params[:uri].should == nil
+      end
     end
-  end
   
-  describe "when receiving do_request" do
-    it "should have access to params " do
-      subject.class.invite(/sip:(.*):(.*)@([^;]*)(;([^;=]*)=([^;=]*))?/) do
-        must_be_called
-      end
-      subject.should_receive(:must_be_called)
-      subject.do_request
+    describe "when receiving do_request" do
+      it "should have access to params " do
+        subject.class.invite(/sip:(.*):(.*)@([^;]*)(;([^;=]*)=([^;=]*))?/) do
+          must_be_called
+        end
+        subject.should_receive(:must_be_called)
+        subject.do_request
       
-      subject.params[:uri][0].should == "sip:+uri-1-2-3:pass@domain.com;params1=test"
-      subject.params[:uri][1].should == "+uri-1-2-3"
-      subject.params[:uri][2].should == "pass"
-      subject.params[:uri][3].should == "domain.com"
-      subject.params[:uri][4].should == ";params1=test"
-      subject.params[:uri][5].should == "params1"
-      subject.params[:uri][6].should == "test"
-      subject.params[:uri][7].should == nil
+        subject.params[:uri].should == %w(sip:+uri-1-2-3:pass@domain.com;params1=test +uri-1-2-3 
+          pass domain.com ;params1=test params1 test)
+      end
     end
   end
-end
 
 
-describe 'Sipatra::Base params with conditions ' do
+  describe 'params with conditions' do
   
-  subject do
-    app_class = Class::new(Sipatra::Base)
-    app = app_class.new
-    app.msg = mock_request('INVITE', 'sip:user@domain.com', 'sip:user:pass@domain.com')
-    app
-  end
+    before do
+      @app.msg = mock_request('INVITE', 'sip:user@domain.com', 'sip:user:pass@domain.com')
+    end
   
-  describe "when receiving do_request" do
-    it "should have access to params with conditions " do
-      subject.class.invite(/sip:(.*)@(.*)/, :Header => /sip:(.*):(.*)@(.*)/, :Header2 => /sip:(.*)@(.*)/, :Header3 => /tel:(.*)/) do
-        must_be_called
-      end
-      subject.should_receive(:must_be_called)
-      subject.do_request
+    describe "when receiving do_request" do
+      it "should have access to params with conditions " do
+        subject.class.invite(/sip:(.*)@(.*)/, 
+          :Header1 => /sip:(.*):(.*)@(.*)/, 
+          :Header2 => /sip:(.*)@(.*)/, 
+          :Header3 => /tel:(.*)/) do
+          must_be_called
+        end
+        subject.should_receive(:must_be_called)
+        subject.do_request
       
-      subject.params.size.should == 3
-      subject.params[:Header].size.should == 4
-      subject.params[:Header2].size.should == 3
-      subject.params[:Header3].should == nil
-      subject.params[:uri][1].should == "user"
-      subject.params[:Header][0].should == "sip:user:pass@domain.com"
-      subject.params[:Header][1].should == "user"
-      subject.params[:Header][2].should == "pass"
-      subject.params[:Header][3].should == "domain.com"
-      subject.params[:Header2][0].should == "sip:user:pass@domain.com"
-      subject.params[:Header2][1].should == "user:pass"
-      subject.params[:Header2][2].should == "domain.com"
+        subject.params.size.should == 3
+        subject.params[:uri].should == %w(sip:user@domain.com user domain.com)
+        subject.params[:Header1].should == %w(sip:user:pass@domain.com user pass domain.com)
+        subject.params[:Header2].should == %w(sip:user:pass@domain.com user:pass domain.com)
+        subject.params[:Header3].should == nil
+      end
     end
   end
-end
 
-describe 'Sipatra::Base responses with params' do
+  describe 'responses with params' do
   
-  subject do
-    app_class = Class::new(Sipatra::Base)
-    app = app_class.new
-    app.msg = mock_response('INVITE', 200, 'sip:user:pass@domain.com')
-    app
-  end
+    before do
+      @app.msg = mock_response('INVITE', 200, 'sip:user:pass@domain.com')
+    end
   
-  it 'should not have an empty size by default' do
-    subject.params.size.should == 0
-  end
+    it 'should have an empty size by default' do
+      subject.params.size.should == 0
+    end
   
-  describe "when receiving do_response" do
-    after do
-      subject.do_response
-    end
-    
-    it "should pass processing through a response line " do
-      subject.class.response do
-        must_be_called
+    describe "when receiving do_response" do
+      after do
+        subject.do_response
       end
-      
-      subject.should_receive(:must_be_called)
-    end
     
-    it "should pass processing through a response line with the right method " do
-      subject.class.response(:INVITE) do
-        must_be_called
-      end
+      it "should pass processing through a response line " do
+        subject.class.response do
+          must_be_called
+        end
       
-      subject.should_receive(:must_be_called)
-    end
+        subject.should_receive(:must_be_called)
+      end
     
-    it "should not pass processing through a response line with the wrong method " do
-      subject.class.response(:REGISTER) do
-        must_not_be_called
-      end
+      it "should pass processing through a response line with the right method " do
+        subject.class.response(:INVITE) do
+          must_be_called
+        end
       
-      subject.should_not_receive(:must_not_be_called)
-    end
+        subject.should_receive(:must_be_called)
+      end
     
-    it "should pass processing through a response line with the right status code " do
-      subject.class.response(:INVITE, 200) do
-        must_be_called
-      end
+      it "should not pass processing through a response line with the wrong method " do
+        subject.class.response(:REGISTER) do
+          must_not_be_called
+        end
       
-      subject.should_receive(:must_be_called)
-    end
+        subject.should_not_receive(:must_not_be_called)
+      end
     
-     it "should not pass processing through a response line with a wrong status code " do
-      subject.class.response(:INVITE, 400) do
-        must_not_be_called
-      end
+      it "should pass processing through a response line with the right status code " do
+        subject.class.response(:INVITE, 200) do
+          must_be_called
+        end
       
-      subject.should_not_receive(:must_not_be_called)
-    end
+        subject.should_receive(:must_be_called)
+      end
     
-     it "should pass processing through a response line with the right status code and no method " do
-      subject.class.response(200) do
-        must_be_called
-      end
+       it "should not pass processing through a response line with a wrong status code " do
+        subject.class.response(:INVITE, 400) do
+          must_not_be_called
+        end
       
-      subject.should_receive(:must_be_called)
-    end
+        subject.should_not_receive(:must_not_be_called)
+      end
     
-    it "should pass processing through a response line with the wrong status code and no method " do
-      subject.class.response(600) do
-        must_not_be_called
-      end
+       it "should pass processing through a response line with the right status code and no method " do
+        subject.class.response(200) do
+          must_be_called
+        end
       
-      subject.should_not_receive(:must_not_be_called)
-    end
+        subject.should_receive(:must_be_called)
+      end
     
-    it "should pass processing through a response line with the wrong status code and no method " do
-      subject.class.response(300, :Header => /sip:(.*):(.*)@(.*)/) do
-        must_not_be_called
-      end
+      it "should pass processing through a response line with the wrong status code and no method " do
+        subject.class.response(600) do
+          must_not_be_called
+        end
       
-      subject.should_not_receive(:must_not_be_called)
+        subject.should_not_receive(:must_not_be_called)
+      end
+    
+      it "should pass processing through a response line with the wrong status code and no method " do
+        subject.class.response(300, :Header => /sip:(.*):(.*)@(.*)/) do
+          must_not_be_called
+        end
+      
+        subject.should_not_receive(:must_not_be_called)
+      end
     end
-  end
   
-  describe "when receiving do_response" do
-    it "should have access to params " do
-      subject.class.response(:INVITE, 200, :Header => /sip:(.*):(.*)@(.*)/, :Header2 => /sip:(.*)@(.*)/, :Header3 => /tel:(.*)/) do
-        must_be_called
-      end
-      subject.should_receive(:must_be_called)
-      subject.do_response
+    describe "when receiving do_response" do
+      it "should have access to params " do
+        subject.class.response :INVITE, 200, 
+          :Header1 => /sip:(.*):(.*)@(.*)/, 
+          :Header2 => /sip:(.*)@(.*)/, 
+          :Header3 => /tel:(.*)/ do
+            must_be_called
+        end
+        subject.should_receive(:must_be_called)
+        subject.do_response
       
-      subject.params.size.should == 2
-      subject.params[:Header].size.should == 4
-      subject.params[:Header2].size.should == 3
-      subject.params[:Header3].should == nil
-      subject.params[:Header][0].should == "sip:user:pass@domain.com"
-      subject.params[:Header][1].should == "user"
-      subject.params[:Header][2].should == "pass"
-      subject.params[:Header][3].should == "domain.com"
-      subject.params[:Header2][0].should == "sip:user:pass@domain.com"
-      subject.params[:Header2][1].should == "user:pass"
-      subject.params[:Header2][2].should == "domain.com"
-    end
+        subject.params.size.should == 2
+        subject.params[:Header1].should == %w(sip:user:pass@domain.com user pass domain.com)
+        subject.params[:Header2].should == %w(sip:user:pass@domain.com user:pass domain.com)
+        subject.params[:Header3].should == nil
+      end
     
-    it "should have access to params when no status nor method are set " do
-      subject.class.response(:Header => /sip:(.*):(.*)@.*/, :Header2 => /sip:(.*)@.*/, :Header3 => /tel:(.*)/) do
-        must_be_called
-      end
-      subject.should_receive(:must_be_called)
-      subject.do_response
+      it "should have access to params when no status nor method are set " do
+        subject.class.response(:Header => /sip:(.*):(.*)@.*/, :Header2 => /sip:(.*)@.*/, :Header3 => /tel:(.*)/) do
+          must_be_called
+        end
+        subject.should_receive(:must_be_called)
+        subject.do_response
       
-      subject.params.size.should == 2
-      subject.params[:Header].size.should == 3
-      subject.params[:Header2].size.should == 2
-      subject.params[:Header3].should == nil
-    end
+        subject.params.size.should == 2
+        subject.params[:Header].size.should == 3
+        subject.params[:Header2].size.should == 2
+        subject.params[:Header3].should == nil
+      end
     
-    it "should have access to params only status is set " do
-      subject.class.response(200, :Header => /sip:(.*):(.*)@.*/, :Header4 => /sip:(.*)@(.*)/) do
-        must_be_called
-      end
-      subject.should_receive(:must_be_called)
-      subject.do_response
+      it "should have access to params only status is set " do
+        subject.class.response(200, :Header => /sip:(.*):(.*)@.*/, :Header4 => /sip:(.*)@(.*)/) do
+          must_be_called
+        end
+        subject.should_receive(:must_be_called)
+        subject.do_response
       
-      subject.params.size.should == 2
-      subject.params[:Header].size.should == 3
-      subject.params[:Header4].size.should == 3
-    end
+        subject.params.size.should == 2
+        subject.params[:Header].size.should == 3
+        subject.params[:Header4].size.should == 3
+      end
+    end  
   end
 end

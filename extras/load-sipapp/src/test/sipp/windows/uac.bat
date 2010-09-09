1edd4b13a54e9f137ba303ec00659486fc5ac109
nicolas@cipango.org <nicolas@cipango.org@657f710f-be33-0410-a596-71a6d81f68ef>
2010-09-09 14:07:18 +0000

    minor
    git-svn-id: https://cipango.googlecode.com/svn/trunk@430 657f710f-be33-0410-a596-71a6d81f68ef

--- extras/load-sipapp/src/test/sipp/windows/uac.bat
+++ extras/load-sipapp/src/test/sipp/windows/uac.bat
@@ -4,7 +4,7 @@
 @IF "%1" == "" goto USAGE
 @IF "%2" == "" goto USAGE
 
-%SIPP_EXE% %SIPP_HOST%:%SIPP_UAS_PORT% -m %NB_MESSAGE% -r %LOAD_CALL_RATE% -rp %RATE_PERIOD%  -i %SIPP_HOST% -p %SIPP_UAC_PORT% %SIPP_OPTIONS% %TRACE_OPTIONS% -sf ..\uac.xml  -rsa %AS_HOST%:%AS_PORT% %1 %2 %3 %4 %5 %6
+%SIPP_EXE% %SIPP_HOST%:%SIPP_UAS_PORT% -m %NB_MESSAGE% -r %LOAD_CALL_RATE% -rp %RATE_PERIOD%  -i %SIPP_HOST% -p %SIPP_UAC_PORT% %SIPP_OPTIONS% %TRACE_OPTIONS% -sf ..\uac.xml  -rsa %AS% %1 %2 %3 %4 %5 %6
 
 @goto DONE
 

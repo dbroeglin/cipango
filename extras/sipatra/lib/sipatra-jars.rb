4fa9eb0f62b37769b935a119c23a27311cb9d1b6
dominique.broeglin <dominique.broeglin@657f710f-be33-0410-a596-71a6d81f68ef>
2010-11-30 15:28:27 +0000

    Adding JAR dependencies to the gem
    git-svn-id: https://cipango.googlecode.com/svn/trunk@563 657f710f-be33-0410-a596-71a6d81f68ef

--- extras/sipatra/lib/sipatra-jars.rb
+++ extras/sipatra/lib/sipatra-jars.rb
@@ -1,16 +1,13 @@
 module SipatraJars
   PATH = File.expand_path(File.dirname(__FILE__))
-  puts PATH
-  puts Dir[PATH + "/sipatra/*"].to_a.inspect
-  JarPath = Dir[PATH + "/sipatra/*"].find { |f| f =~ %r{/sipatra-.*\.jar$} }
 
-  class << self 
+  class << self
     def all_jar_names
-      @all_jar_names ||= [File::basename(JarPath)]
+      @all_jar_names ||= all_jar_paths.map { |path| File::basename(path) }
     end
-    
+
     def all_jar_paths
-      @all_jar_paths ||= [JarPath]
+      @all_jar_paths ||= Dir[PATH + "/sipatra/*.jar"]
     end
   end
 end
